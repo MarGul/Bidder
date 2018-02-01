81786b42f54fe088c777a441f79768aa9f69a691
Marcus Gullberg <gullbeerg@gmail.com>
2018-02-01 19:31:57 +0000

    Base layout completed for mail settings

--- resources/assets/js/components/User/Notifications/NotificationSettings.vue
+++ resources/assets/js/components/User/Notifications/NotificationSettings.vue
@@ -1,75 +1,52 @@
 <template>
-	<div class="notification_settings-component">
-		<section class="white-contentSection">
-			<header class="white-contentSection-header">
-				<h3>Inställningar för notifikationer</h3>
-			</header>
-			<div class="white-contentSection-content">
-				<template v-if="fetched">
-
-					<app-notification-settings-item v-for="(value, setting) in settings" :key="setting"
-						:setting="setting"
-						:value="value"
-						@updated="settingChanged">		
-					</app-notification-settings-item>
-					
-				</template>
-
-				<app-loading v-else></app-loading>
-			</div>
-			<footer class="white-contentSection-footer">
-				<button type="button" class="btn btn-primary" :class="{processing}" :disabled="processing" @click.prevent="update">
-					Uppdatera
-				</button>
-			</footer>
-		</section>
-	</div>
+    <section class="transparent-contentSection">
+        <header class="transparent-contentSection-header has-link">
+            <h3>Mail-inställningar</h3>
+        </header>
+        <div class="transparent-contentSection-content">
+            <ul class="notification-settings-list">
+                <li>
+                    <div class="notification-setting-header">
+                        <div class="notification-setting-title">
+                            För mina tjänster
+                        </div>
+                        <div class="notification-setting-control">
+                            <app-checkbox />
+                        </div>
+                    </div>
+                    <div class="notification-setting-content">
+                        Få ett email så fort något händer på dina tjänster. Exempelvis en ny
+                        kommentar, nytt bud eller när din tjänst håller på att gå ut.
+                    </div>
+                </li>
+                <li>
+                    <div class="notification-setting-header">
+                        <div class="notification-setting-title">
+                            För mina projekt
+                        </div>
+                        <div class="notification-setting-control">
+                            <app-checkbox />
+                        </div>
+                    </div>
+                    <div class="notification-setting-content">
+                        Få ett email så fort något händer på dina projekt. Exempelvis när 
+                        detaljerna uppdateras eller den andra parten godkänner projektet.
+                    </div>
+                </li>
+            </ul>
+            <button type="button" class="btn btn-primary full-width c_c">
+                Uppdatera inställningarna
+            </button>
+        </div>
+    </section>
 </template>
 
 <script>
-	import { mapGetters } from 'vuex';
-	import appNotificationSettingsItem from "./NotificationSettingsItem";
-	import Model from "../../../includes/Model";
+    import appCheckbox from '../../InputControls/Checkbox';
 
-	export default {
-		components: {
-			appNotificationSettingsItem
-		},
-		data() {
-			return {
-				processing: false
-			}
-		},
-		computed: {
-			...mapGetters({
-				fetched: 'userNotificationSettingsFetched',
-				settings: 'userNotificationSettings'
-			})
-		},
-		methods: {
-			settingChanged(event) {
-				this.settings[event.setting] = Boolean(!this.settings[event.setting]);
-			},
-			update() {
-				this.processing = true;
-				new Model('users/notification-settings').patch(this.settings)
-					.then(response => {
-						this.$store.commit('SET_USER_NOTIFICATION_SETTINGS', {notificationSettings: this.settings});
-						this.$store.dispatch('showNotification', {type: 'success', msg: 'Vi har uppdaterat dina inställningar för när du ska få notifikationer.'});
-						this.processing = false;
-					})
-					.catch(error => console.log(error));
-			}
-		},
-		created() {
-			if ( !this.fetched ) {
-				new Model('user/notification-settings').get()
-					.then(response => {
-						this.$store.commit('SET_USER_NOTIFICATION_SETTINGS', response.data.settings);
-						this.$store.commit('SET_USER_NOTIFICATION_SETTINGS_FETCHED', true);
-					})
-					.catch(error => { console.log(error); });
-			}
-		}
-	}
+    export default {
+        components: {
+            appCheckbox
+        }
+    }
 </script>
--- resources/assets/js/components/User/Notifications/Notifications.vue
+++ resources/assets/js/components/User/Notifications/Notifications.vue
@@ -1,15 +1,30 @@
 <template>
-	<div class="notifications-component">
-		<app-notification-settings></app-notification-settings>
-	</div>
+    <div class="notifications_component">
+        <div class="main-area-with-sidebar">
+            <div class="main-area">
+                <section class="white-contentSection">
+                    <header class="white-contentSection-header">
+                        <h3>Notifikationer</h3>
+                    </header>
+                    <div class="white-contentSection-content">
+                        
+                    </div>
+                </section>
+            </div>
+            <div class="main-area-sidebar">
+                <app-notification-settings />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	import appNotificationSettings from './NotificationSettings';
+    import appNotificationSettings from './NotificationSettings';
 
-	export default {
-		components: {
-			appNotificationSettings
-		}
-	}
+    export default {
+        components: {
+            appNotificationSettings
+        }
+    }
 </script>
+
