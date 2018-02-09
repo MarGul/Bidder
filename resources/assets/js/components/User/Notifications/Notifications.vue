d04de618d1b8e3b5dd3bbcbd15ce37bc0425cd2a
Marcus Gullberg <gullbeerg@gmail.com>
2018-02-09 17:05:47 +0000

    Showing the notifications at show all

--- resources/assets/js/components/User/Notifications/Notifications.vue
+++ resources/assets/js/components/User/Notifications/Notifications.vue
@@ -3,11 +3,23 @@
         <div class="main-area-with-sidebar">
             <div class="main-area">
                 <section class="white-contentSection">
-                    <header class="white-contentSection-header">
+                    <header class="white-contentSection-header has-link">
                         <h3>Notifikationer</h3>
+                        <a class="is-link" @click="markRead">Markera alla som lästa</a>
                     </header>
                     <div class="white-contentSection-content">
-                        
+                        <app-loading v-if="!fetched" />
+
+                        <template v-else>
+                            <app-notification 
+                                v-for="(notification, index) in notifications" 
+                                :key="index" 
+                                :data="Object.assign(notification.data, {read_at: notification.read_at, created_at: notification.created_at})"
+                                v-if="notifications.length"
+                            />
+
+                            <div class="alert alert-info text-center m0" v-else>Du har inga notifieringar än.</div>
+                        </template>
                     </div>
                 </section>
             </div>
@@ -20,10 +32,24 @@
 
 <script>
     import appNotificationSettings from './NotificationSettings';
+    import { mapGetters } from 'vuex';
+    import appNotification from '../../Includes/OneNotification';
 
     export default {
         components: {
-            appNotificationSettings
+            appNotificationSettings,
+            appNotification
+        },
+        computed: {
+            ...mapGetters({
+                fetched: 'userNotificationsFetched',
+                notifications: 'userNotifications'
+            })
+        },
+        methods: {
+            markRead(){
+                this.$store.dispatch('markNotificationsAsRead');
+            }
         }
     }
 </script>
