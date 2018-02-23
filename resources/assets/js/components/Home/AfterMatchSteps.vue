4be06d884e800aa0ac3af1b09eaa942de5be8124
Marcus Gullberg <gullbeerg@gmail.com>
2018-02-23 20:21:12 +0000

    Home page features exerpt created

---
+++ resources/assets/js/components/Home/AfterMatchSteps.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="home-handle-service-steps-outer after-match">
+        <div class="container">
+            <div class="handle-service-steps-heading-container">
+                <span>Vad händer sen?</span>
+            </div>
+
+            <div class="handle-service-steps-container">
+                <div class="handle-service-step">
+                    <div class="handle-service-step-icon icon-project-created"></div>
+                    <h4 class="handle-service-step-heading">Ett projekt skapas</h4>
+                    <div class="handle-service-step-content">
+                        När en tjänst och ett bud har matchats så skapas ett projekt åt er. Här har ni en 14 dagars period att
+                        lära känna varandra lite mer. Om allt ser bra ut så accepterar ni projektets start.
+                    </div>
+                </div>
+
+                <div class="handle-service-step">
+                    <div class="handle-service-step-icon icon-use-contract"></div>
+                    <h4 class="handle-service-step-heading">Använd ett avtal</h4>
+                    <div class="handle-service-step-content">
+                        Använd vårt framtagna avtal direkt i portalen om ni vill. Detta är inget tvång men vid större uppdrag 
+                        så kan det vara en skön känsla att ha avtal på vad som ska göras.
+                    </div>
+                </div>
+
+                <div class="handle-service-step">
+                    <div class="handle-service-step-icon icon-leave-review"></div>
+                    <h4 class="handle-service-step-heading">Lämna omdöme</h4>
+                    <div class="handle-service-step-content">
+                        När jobbet är slutfört så får ni möjligheten att betygsätta varandra. Med hjälp av vårt omdömessystem så
+                        kommer dåliga aktörer på plattformen att försvinna.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
---
+++ resources/assets/js/components/Home/CreateServiceSteps.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="home-handle-service-steps-outer create-service">
+        <div class="container">
+            <div class="handle-service-steps-heading-container">
+                <span>Vill du få en tjänst utförd?</span>
+            </div>
+
+            <div class="handle-service-steps-container">
+                <div class="handle-service-step">
+                    <div class="handle-service-step-icon icon-create-service"></div>
+                    <h4 class="handle-service-step-heading">Skapa din tjänst</h4>
+                    <div class="handle-service-step-content">
+                        Beskriv tjänsten som du vill få utförd. Lägg till bilder, ritningar eller annan media. Berätta 
+                        när du vill att tjänsten ska påbörjas och när den ska vara färdig.
+                    </div>
+                </div>
+
+                <div class="handle-service-step">
+                    <div class="handle-service-step-icon icon-get-bids"></div>
+                    <h4 class="handle-service-step-heading">Få bud på att utföra tjänsten</h4>
+                    <div class="handle-service-step-content">
+                        Ta emot bud från företag på att få utföra din tjänst. Istället för att du ska ringa runt och 
+                        samla in offerter så låter du offerterna komma till dig. 
+                    </div>
+                </div>
+
+                <div class="handle-service-step">
+                    <div class="handle-service-step-icon icon-choose-service"></div>
+                    <h4 class="handle-service-step-heading">Välj det budet som passar dig</h4>
+                    <div class="handle-service-step-content">
+                        När budgivningen är färdig är det dags för dig att välja vem du vill ska få utföra tjänsten. 
+                        Ta hjälp av vårt omdömessystem för att hitta rätt. Du har kontrollen själv.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
