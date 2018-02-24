f56a663c41d735702ee6dd2660c894fe9aafd96b
Marcus Gullberg <gullbeerg@gmail.com>
2018-02-24 13:57:51 +0000

    Controller, route and manager is setup

---
+++ app/Managers/FeedbackManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Managers;
+
+use App\Feedback;
+
+class FeedbackManager extends BaseManager
+{
+
+
+    /**
+     * Create a feedback entry
+     *
+     * @param   array       $data
+     * @return  boolean
+     */
+    public function create($data)
+    {
+        dd($data);
+    }
+
+}
