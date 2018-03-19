547fc9a7774d9e7d86472daebdbca5152ec35097
Marcus Gullberg <gullbeerg@gmail.com>
2018-03-19 20:37:44 +0000

    Started working on the mobile menu

---
+++ resources/assets/js/components/Layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="site-header">
+        <div class="mobile-menu-toggle" @click="toggleMobileMenu"><span></span></div>
+        <div class="logo-container">
+            <img src="/images/logo.png" alt="GoBid Logo" />
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+    methods: {
+        toggleMobileMenu() {
+            if ( document.body.classList.contains('mobile-menu-open') ) {
+                document.body.classList.remove('mobile-menu-open');
+            } else {
+                document.body.classList.add('mobile-menu-open');
+            }
+        }
+    }
+}
+</script>
+
---
+++ resources/assets/js/components/Layout/MobileMenu.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mobile-menu">
+        <img src="/images/logo.png">
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br>
+        Menu from http://shiftnav.io/
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
